1ed266e0bf37e30578deb79d371e4c957c0f51a8
crayon <lsh328328@naver.com>
2022-06-10 16:31:44 +0900

    7th_Seminar Level 1

---
+++ 7th_Seminar/src/controllers/index.ts
@@ -0,0 +1,6 @@
+import UserController from "./UserController";
+import MovieController from "./MovieController";
+import ReviewController from "./ReviewController";
+import FileController from "./FileController";
+// controller index file
+export { UserController, MovieController, ReviewController, FileController };
---
+++ 7th_Seminar/src/services/index.ts
@@ -0,0 +1,6 @@
+import UserService from "./UserService";
+import MovieService from "./MovieService";
+import ReviewService from "./ReviewService";
+import FileService from "./FileService";
+//service index file
+export { UserService, MovieService, ReviewService, FileService };
